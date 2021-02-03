8120a2a33dcbb8f39ef7e83e419b8543a0c9c3d0
Matthew Gleich <email@mattglei.ch>
2021-02-03 09:44:05 -0500

    🔊 Make db connection failures fatal
    Signed-off-by: Matthew Gleich <email@mattglei.ch>

--- pkg/db/con.go
+++ pkg/db/con.go
@@ -29,11 +29,11 @@ func Connect() {
 		dbURL.Port(),
 	)
 	DB, err = sql.Open("postgres", postgresInfo)
-	lumber.Error(err, "Failed to validate to postgres info:", postgresInfo)
+	lumber.Fatal(err, "Failed to validate to postgres info:", postgresInfo)
 
 	// Verifying connection to database
 	err = DB.Ping()
-	lumber.Error(err, "Failed to connect to database")
+	lumber.Fatal(err, "Failed to connect to database")
 
 	lumber.Info("Connected to database")
 }
