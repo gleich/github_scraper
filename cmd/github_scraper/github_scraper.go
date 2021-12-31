ad46f1ef04a6af62ac5fadbd6ca6c6dc09d4b62a
Matt Gleich <git@mattglei.ch>
2021-12-31 16:23:06 -0500

    feat: run every 5 minutes in prod
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- cmd/github_scraper/github_scraper.go
+++ cmd/github_scraper/github_scraper.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"os"
+	"time"
+
 	"github.com/gleich/github_scraper/pkg/api"
 	"github.com/gleich/github_scraper/pkg/db"
 	"github.com/gleich/lumber/v2"
+	"github.com/go-co-op/gocron"
+	"github.com/shurcooL/githubv4"
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -19,9 +25,36 @@ func main() {
 
 	client := api.Client()
 
-	init_account, err := api.Account(client)
+	account, err := api.Account(client)
 	if err != nil {
 		lumber.Fatal(err, "Failed to fetch initial account information")
 	}
-	database.Create(&init_account)
+	database.Create(&account)
+
+	s := gocron.NewScheduler(time.UTC)
+
+	scheduledCycle := func() { cycle(database, client, &account) }
+	if os.Getenv("DEV") == "true" {
+		_, err := s.Every(5).Seconds().Do(scheduledCycle)
+		if err != nil {
+			lumber.Error(err, "Failed to start scheduled cycle for every 5 seconds")
+		}
+	} else {
+		_, err := s.Every(5).Minutes().Do(scheduledCycle)
+		if err != nil {
+			lumber.Error(err, "Failed to start scheduled cycle for every 5 minutes")
+		}
+	}
+
+	s.StartBlocking()
+}
+
+func cycle(database *gorm.DB, client *githubv4.Client, account *db.Account) {
+	new_account_data, err := api.Account(client)
+	if err != nil {
+		lumber.Error(err, "Failed to load new account data")
+	}
+	database.Model(&account).Updates(new_account_data)
+
+	lumber.Success("Cycle completed")
 }
